16dd26bed224d76cc6820709b043177f769a7559
Ndrs03 <kaa.alexandersson@outlook.com>
2023-05-11 13:47:16 +0200

    Lade till andra sensorn

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.IN)
+
 
 # Delay är satt så lågt som möjligt på sensorn vilket är ca 15s så minimum inspelningstid är ca 15s annars startas en till filmning innan sensorn hinner bli 0
 
 while True:
 
-	if GPIO.input(20):
+	if GPIO.input(20) or GPIO.input(21):
 		print("Sensor detected")
 		record.start_record(20)	# filma 20 sek
 		stats.update_csv()
